ac4acccf5c69d9cd6d4a3e81dbd18468999cfc70
Paride Zavoli <paride.zavoli@outlook.com>
2024-12-30 11:42:13 +0100

    Gestione con scelta file ingresso e uscita

--- GCodePanelizer/Form1.Designer.cs
+++ GCodePanelizer/Form1.Designer.cs
@@ -28,23 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btCreaPannello = new Button();
             txNumRighe = new TextBox();
             lbRighe = new Label();
             label1 = new Label();
             txNumColonne = new TextBox();
             txDistanzaX = new TextBox();
+            txDistanzay = new TextBox();
+            txCorpo = new TextBox();
+            txHeader = new TextBox();
+            txFooter = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            btLeggiFile = new Button();
             SuspendLayout();
             // 
-            // button1
+            // btCreaPannello
             // 
-            button1.Location = new Point(38, 82);
-            button1.Name = "button1";
-            button1.Size = new Size(166, 37);
-            button1.TabIndex = 0;
-            button1.Text = "Crea Pannello";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            btCreaPannello.Location = new Point(599, 35);
+            btCreaPannello.Name = "btCreaPannello";
+            btCreaPannello.Size = new Size(166, 23);
+            btCreaPannello.TabIndex = 0;
+            btCreaPannello.Text = "Crea Pannello";
+            btCreaPannello.UseVisualStyleBackColor = true;
+            btCreaPannello.Click += btCreaPannello_Click;
             // 
             // txNumRighe
             // 
@@ -67,7 +74,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(38, 40);
+            label1.Location = new Point(38, 43);
             label1.Name = "label1";
             label1.Size = new Size(52, 15);
             label1.TabIndex = 3;
@@ -84,24 +91,98 @@
             // 
             // txDistanzaX
             // 
-            txDistanzaX.Location = new Point(229, 6);
+            txDistanzaX.Location = new Point(332, 6);
             txDistanzaX.Name = "txDistanzaX";
-            txDistanzaX.Size = new Size(100, 23);
+            txDistanzaX.Size = new Size(64, 23);
             txDistanzaX.TabIndex = 5;
             txDistanzaX.Text = "25";
             txDistanzaX.TextAlign = HorizontalAlignment.Right;
+            // 
+            // txDistanzay
+            // 
+            txDistanzay.Location = new Point(332, 40);
+            txDistanzay.Name = "txDistanzay";
+            txDistanzay.Size = new Size(64, 23);
+            txDistanzay.TabIndex = 6;
+            txDistanzay.Text = "25";
+            txDistanzay.TextAlign = HorizontalAlignment.Right;
+            // 
+            // txCorpo
+            // 
+            txCorpo.AcceptsReturn = true;
+            txCorpo.Location = new Point(12, 246);
+            txCorpo.MaxLength = 300000;
+            txCorpo.Multiline = true;
+            txCorpo.Name = "txCorpo";
+            txCorpo.ScrollBars = ScrollBars.Both;
+            txCorpo.Size = new Size(753, 388);
+            txCorpo.TabIndex = 7;
+            txCorpo.WordWrap = false;
+            // 
+            // txHeader
+            // 
+            txHeader.Location = new Point(12, 69);
+            txHeader.Multiline = true;
+            txHeader.Name = "txHeader";
+            txHeader.ScrollBars = ScrollBars.Both;
+            txHeader.Size = new Size(753, 170);
+            txHeader.TabIndex = 8;
+            // 
+            // txFooter
+            // 
+            txFooter.Location = new Point(12, 640);
+            txFooter.Multiline = true;
+            txFooter.Name = "txFooter";
+            txFooter.ScrollBars = ScrollBars.Both;
+            txFooter.Size = new Size(753, 108);
+            txFooter.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(189, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(115, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Larghezza riga (mm)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(189, 43);
+            label3.Name = "label3";
+            label3.Size = new Size(138, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Larghezza colonna (mm)";
+            // 
+            // btLeggiFile
+            // 
+            btLeggiFile.Location = new Point(599, 9);
+            btLeggiFile.Name = "btLeggiFile";
+            btLeggiFile.Size = new Size(166, 23);
+            btLeggiFile.TabIndex = 12;
+            btLeggiFile.Text = "Leggi file gcode";
+            btLeggiFile.UseVisualStyleBackColor = true;
+            btLeggiFile.Click += btLeggiFile_Click;
             // 
             // fmGCodePanelizer
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(341, 142);
+            ClientSize = new Size(780, 757);
+            Controls.Add(btLeggiFile);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txFooter);
+            Controls.Add(txHeader);
+            Controls.Add(txCorpo);
+            Controls.Add(txDistanzay);
             Controls.Add(txDistanzaX);
             Controls.Add(txNumColonne);
             Controls.Add(label1);
             Controls.Add(lbRighe);
             Controls.Add(txNumRighe);
-            Controls.Add(button1);
+            Controls.Add(btCreaPannello);
             Name = "fmGCodePanelizer";
             Text = "GCode Panelizer";
             ResumeLayout(false);
@@ -110,11 +191,18 @@
 
         #endregion
 
-        private Button button1;
+        private Button btCreaPannello;
         private TextBox txNumRighe;
         private Label lbRighe;
         private Label label1;
         private TextBox txNumColonne;
         private TextBox txDistanzaX;
+        private TextBox txDistanzay;
+        private TextBox txCorpo;
+        private TextBox txHeader;
+        private TextBox txFooter;
+        private Label label2;
+        private Label label3;
+        private Button btLeggiFile;
     }
 }
--- GCodePanelizer/Form1.cs
+++ GCodePanelizer/Form1.cs
@@ -9,18 +9,54 @@ namespace GCodePanelizer
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btCreaPannello_Click(object sender, EventArgs e)
         {
             try
             {
-                int numcolonne = int.Parse( txNumColonne.Text);
+                int numcolonne = int.Parse(txNumColonne.Text);
                 int numrighe = int.Parse(txNumRighe.Text);
                 double distx = double.Parse(txDistanzaX.Text);
                 distx = Math.Truncate(distx * 100) / 100;
 
+                string intestazione = txHeader.Text;
+                string footer = txFooter.Text;
+                string corpo = txCorpo.Text;
+
+                FileDialog fd = new SaveFileDialog();
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    using (var writer = new StreamWriter(File.OpenWrite(fd.FileName), Encoding.UTF8))
+                    {
+                        writer.Write(intestazione);
+                        for (int i = 0; i < numrighe; i++)
+                        {
+                            for (int j = 0; j < numcolonne; j++)
+                            {
+                                writer.WriteLine("\r\n;Pannello " + i + " " + j);
+                                if (!(j == 0))
+                                    writer.WriteLine("G0 X" + distx + " Y0");
+                                writer.WriteLine("G92 X0 Y0");
+                                writer.Write(corpo);
+                            }
+                            if ( !( i == numrighe - 1 ))
+                                writer.WriteLine("G0 X-" + (distx * (numcolonne - 1)) + " Y" + (distx));
+                        }
+                        writer.Write("\r\n" + footer);
+                    }
+                }
+
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void btLeggiFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
                 StringBuilder st = new StringBuilder(1000000);
-                int offset = 0, conta = 0;
-                byte []bt = new byte[2000000];
                 FileDialog fd = new OpenFileDialog();
                 fd.CheckFileExists = true;
                 if (fd.ShowDialog() == DialogResult.OK)
@@ -32,28 +68,13 @@ namespace GCodePanelizer
                             while (!reader.EndOfStream)
                             {
                                 var line = reader.ReadLine();
-                                st.Append(line + "\n");
+                                st.Append(line + "\r\n");
                             }
                         }
-                        using (var writer = new StreamWriter(File.OpenWrite(@"c:\\temp\\pannello_gcode.gcode"), Encoding.UTF8))
-                        { 
-                            for (int i = 0; i < numrighe; i++)
-                            {
-                                for (int j = 0; j < numcolonne; j++)
-                                {
-                                    writer.WriteLine(";Pannello " + i + " " + j);
-                                    if (! (j == 0)) 
-                                        writer.WriteLine("G0 X" + distx + " Y0");
-                                    writer.WriteLine("G92 X0 Y0");
-                                    writer.Write(st.ToString());
-                                }
-                                writer.WriteLine("G0 X-" + (distx * numcolonne) + " Y" + (distx * 2));
-                            }
-                        }
+                        txCorpo.Text = st.ToString();
                     }
-                    
                 }
-            } catch(Exception ee)
+            } catch (Exception ee)
             {
                 MessageBox.Show(ee.Message);
             }
